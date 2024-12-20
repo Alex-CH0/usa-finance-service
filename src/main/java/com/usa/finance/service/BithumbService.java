1123e00f6673326f415ce970ae254f2339bead90
Alex-CHO <hyungjin98@gmail.com>
2024-12-20 18:42:00 +0900

    Todo note added

--- src/main/java/com/usa/finance/service/BithumbService.java
+++ src/main/java/com/usa/finance/service/BithumbService.java
@@ -10,5 +10,10 @@ public class BithumbService {
 
     private final BithumbServiceClient bithumbServiceClient;
 
+    // TODO jpa 연결하고, DB 구축 후 DB에 데이터 넣을 생각 해보자....
+    // TODO 1. DB 로컬에서 띄우기.
+    // TODO 2. JPA 및 repository 생성
+    // TODO 3. Scheduled 로 ticker 조회 및 저장.
+    // TODO 4.
 
 }
