16950ca4397e66e2874f40286a9e2004c3be1978
Alex-CHO <hyungjin98@gmail.com>
2025-02-04 14:03:29 +0900

    import optimized

--- src/main/java/com/usa/finance/service/BithumbService.java
+++ src/main/java/com/usa/finance/service/BithumbService.java
@@ -10,7 +10,6 @@ public class BithumbService {
 
     private final BithumbServiceClient bithumbServiceClient;
 
-    // TODO 2. JPA 및 repository 테스트 및 테스트 코드 작성.
     // TODO 3. Scheduled 로 ticker 조회 및 저장.
     // TODO 4.
 
--- src/test/java/com/usa/finance/repository/RepositoryTests.java
+++ src/test/java/com/usa/finance/repository/RepositoryTests.java
@@ -2,8 +2,6 @@ package com.usa.finance.repository;
 
 import com.usa.finance.domain.Market;
 import com.usa.finance.dto.MarketDto;
-import com.usa.finance.service.MarketService;
-import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +10,6 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
